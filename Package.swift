230f39ed7f76c4d5ef6d6966a53748f1ebf463c5
William Archimede <william.archimede@francetv.fr>
2024-01-05 17:40:43 +0100

    chore(SPM): Revert previous attempt at having platform specific products

--- Package.swift
+++ Package.swift
@@ -5,15 +5,14 @@ import PackageDescription
 let package = Package(
     name: "FTVFreewheelKit",
     platforms: [
-        .iOS(.v10), .tvOS(.v12),
+        .iOS(.v10),
+        .tvOS(.v12)
     ],
     products: [
-        .library(name: "FTVFreewheelLib-iOS", targets: ["AdManager-iOS"]),
-        .library(name: "FTVFreewheelLib-tvOS", targets: ["AdManager-tvOS"])
+        .library(name: "FTVFreewheelLib", targets: ["AdManager"])
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "AdManager-iOS", path: "iOS/AdManager.xcframework"),
-        .binaryTarget(name: "AdManager-tvOS", path: "tvOS/AdManager.xcframework")
+        .binaryTarget(name: "AdManager", path: "AdManager.xcframework")
     ]
 )
